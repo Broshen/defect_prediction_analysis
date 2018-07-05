2afc70501a761bc43fa30edf10ae283422490a39
Boshen Cui <boshen.cui@gmail.com>
2018-07-05 17:03:25 -0400

    properly discretized all data columns based on variable distributions

--- format_data.py
+++ format_data.py
@@ -1,117 +1,93 @@
 import pandas as pd
 import os
 
-INPUT_FILE_PATH = '../change_burst_data-master/weekly/packages/'
+INPUT_FOLDER = './preprocessing/aggregated/weekly/classes/'
+OUTPUT_FOLDER = './preprocessing/discretized/weekly/classes/'
+
+BINS = {
+	'bugs': [-1,0,10],
+	'ChurnTotal': [-1,0,10,100,1000,100000],
+	'MaxChurnInBurst': [-1, 0, 10, 100, 1000,100000],
+	'MaximumCodeBurst': [-1,0, 100],
+	'MaximumCodeBurstEarly': [-1,0, 100],
+	'MaximumCodeBurstLate': [-1,0, 100],
+	'MaxPeopleInBurst': [-1, 0, 100],
+	'NumberCodeBursts': [-1, 0, 100],
+	'NumberCodeBurstsEarly': [-1,0, 100],
+	'NumberCodeBurstsLate': [-1, 0, 10],
+	'NumberConsecutiveChanges': [-1,0,1,10, 100],
+	'NumberConsecutiveChangesLate': [-1,0,1,10, 100],
+	'NumberOfChanges': [-1,0,1,2, 100],
+	'NumberOfChangesEarly': [-1,0,1,2, 100],
+	'NumberOfChangesLate': [-1,0,1,2, 100],
+	'NumberOfConsecutiveChangesEarly': [-1,0,1,10, 100],
+	'NumberOfDefects': [-1, 100],
+	'PeopleTotal': [-1, 0, 100],
+	'pre': [-1, 0, 100],
+	'TLOC': [-1, 10, 100, 10000],
+	'TotalBurstSize': [-1, 0, 100],
+	'TotalBurstSizeEarly': [-1, 0, 100],
+	'TotalBurstSizeLate': [-1, 0, 100],
+	'TotalChurnInBurst': [-1, 0, 10, 100, 1000, 100000],
+	'TotalPeopleInBurst': [-1, 0, 100],
+
+	# NOT included:
+
+	# 'TimeFirstBurst': 0-1, rest in equal thirds
+	# 'TimeLastBurst': 0-1, rest in equal thirds
+	# 'TimeMaxBurst': 0-1, rest in equal thirds
+	# GapSize, BurstSize: leave as 10 discrete values
+}
 
 # expects a .csv filename, whose file has ; seperators
-def format_from_burst_file(file_name):
+def format_from_burst_file(file_name, output_folder):
 	print("formatting: " + file_name)
-	data = pd.read_csv(file_name, sep=";")
+	data = pd.read_csv(file_name, sep=" ")
 	df = pd.DataFrame(data)
 
-	# drop filename for now
-	# TODO: bucketize filename by prefix
-	df = df.drop(['filename'], axis=1)
-
-	# drop metadata columns, keep only columns related to bursts
-	# wrap in a try catch block as some files (ECLIPSE31_*) don't
-	# have pre/NumberOfDefects columns, and has bugs column instead
-	try:
-		df = df[[
-		'TotalPeopleInBurst',
-		'MaximumCodeBurstLate',
-		'NumberOfChanges',
-		'MaxPeopleInBurst',
-		'TotalBurstSizeLate',
-		'NumberCodeBurstsLate',
-		'NumberOfChangesLate',
-		'NumberOfChangesEarly',
-		'MaxChurnInBurst',
-		'MaximumCodeBurstEarly',
-		'NumberCodeBurstsEarly',
-		'TimeFirstBurst',
-		'TotalChurnInBurst',
-		'ChurnTotal',
-		'MaximumCodeBurst',
-		'NumberOfConsecutiveChangesEarly',
-		'NumberConsecutiveChangesLate',
-		'TotalBurstSizeEarly',
-		'TotalBurstSize',
-		'TimeMaxBurst',
-		'NumberConsecutiveChanges',
-		'TimeLastBurst',
-		'NumberCodeBursts',
-		'PeopleTotal',
-		'pre',
-		'NumberOfDefects'
-		]]
-	except KeyError:
-		df = df[[
-		'TotalPeopleInBurst',
-		'MaximumCodeBurstLate',
-		'NumberOfChanges',
-		'MaxPeopleInBurst',
-		'TotalBurstSizeLate',
-		'NumberCodeBurstsLate',
-		'NumberOfChangesLate',
-		'NumberOfChangesEarly',
-		'MaxChurnInBurst',
-		'MaximumCodeBurstEarly',
-		'NumberCodeBurstsEarly',
-		'TimeFirstBurst',
-		'TotalChurnInBurst',
-		'ChurnTotal',
-		'MaximumCodeBurst',
-		'NumberOfConsecutiveChangesEarly',
-		'NumberConsecutiveChangesLate',
-		'TotalBurstSizeEarly',
-		'TotalBurstSize',
-		'TimeMaxBurst',
-		'NumberConsecutiveChanges',
-		'TimeLastBurst',
-		'NumberCodeBursts',
-		'PeopleTotal',
-		'bugs'
-		]]
-
 	# discretize all columns to maximum 4 categories
-	for i in df:
-		col = df[i]
-		unique = len(col.unique())
+	for colname in df:
+		print(colname)
+		column = df[colname]
 
-		if unique > 4:
-		 	df[i] = pd.cut(col,bins=4, labels=False)
+		if colname == "GapSize" or colname == "BurstSize":
+			continue
+		elif (colname == "TimeFirstBurst" or
+			colname == "TimeLastBurst" or
+			colname == "TimeMaxBurst"):
+		# do -1, rest in thirds
+			bins=[-2,0,]
+			maxval = column.max()
+			b1 = int(maxval/3)
+			b2 = 2*b1
+			bins.append(b1)
+			bins.append(b2)
+			bins.append(maxval+1)
 
-	output_folder = os.path.dirname(file_name)+"/gobnilp_formatted/"
+			df[colname] = pd.cut(column,bins=bins, labels=False)
+		elif colname in BINS:
+			df[colname] = pd.cut(column,bins=BINS[colname], labels=False)
+		else:
+			print("ERROR: BINS FOR " + colname + "NOT DEFINED")
+
+
 	output_file_name = os.path.basename(file_name).replace('.csv', "-gobnilp_formatted.csv")
 	
 	if not os.path.exists(output_folder):
-		os.mkdir(output_folder)
+		os.makedirs(output_folder)
 
 	df.to_csv(path_or_buf=output_folder+output_file_name, sep=" ", index=False)
 
-def format_change_burst_data(input_path):
-
-	# standardize trailing slash
-	if(input_path[-1] != "/"):
-		input_path+="/"
+def format_change_burst_data(input_path, output_folder):
 
 	if os.path.isdir(input_path):
 		for file_name in os.listdir(input_path):
 
 			subpath = input_path+file_name
-
-			# since the formatted folders may also be in the folder
-			# we are iterating over, we don't want to reformat
-			# the already formatted files
-			if "/gobnilp_formatted/" not in subpath:
-				if os.path.isdir(subpath):
-					format_change_burst_data(subpath)
-				else:
-					format_from_burst_file(subpath)
+			format_change_burst_data(subpath, output_folder)
 	else:
-		format_from_burst_file(input_path)
+		format_from_burst_file(input_path, output_folder)
 		
 
 
-format_change_burst_data(INPUT_FILE_PATH)
+format_change_burst_data(INPUT_FOLDER, OUTPUT_FOLDER)
--- plot_columns.py
+++ plot_columns.py
@@ -53,6 +53,36 @@ def draw_graphs_for_columns(file_name, file_path, output_folder):
 		fig = None
 		colcount = None
 
+def find_max_val_for_columns(file_path, cols):
+	print("working on: " + file_path)
+	data = pd.read_csv(file_path, sep=" ")
+	df = pd.DataFrame(data)
+
+	for colname in df.columns:
+		column = df[[colname]]
+		colcount = column[colname]
+
+		if colname in cols:
+			cols[colname] = max(cols[colname],colcount.max())
+		else:
+			cols[colname] = colcount.max()
+
+def find_max_forall_in_folder(input_path):
+
+	if not os.path.isdir(input_path):
+		print("Error: path specified is not a folder.")
+		return
+
+	cols = {}
+
+	for file_name in os.listdir(input_path):
+		full_path = input_path+file_name
+		if not os.path.isdir(full_path):
+			find_max_val_for_columns(full_path, cols)
+
+	for col in cols:
+		print(col + " : " + str(cols[col]))
+
 def draw_graphs_from_folder(input_path, output_folder):
 
 	if not os.path.isdir(input_path):
@@ -65,4 +95,5 @@ def draw_graphs_from_folder(input_path, output_folder):
 			draw_graphs_for_columns(file_name, full_path, output_folder)
 
 
-draw_graphs_from_folder(INPUT_FOLDER, OUTPUT_FOLDER)
+# draw_graphs_from_folder(INPUT_FOLDER, OUTPUT_FOLDER)
+find_max_forall_in_folder(INPUT_FOLDER)
